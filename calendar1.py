82aca5eaf4883e9810afe157cd595ef6ca77c133
superhuman2020 <70416002+superhuman2020@users.noreply.github.com>
2020-09-18 19:51:49 -0700

    mycalendar
    just part of my school schedule.

---
+++ calendar1.py
@@ -0,0 +1,10 @@
+#Here I will define "mycalendar".
+mycalendar = """
+                            Day           Time.                          Subject
+                            Monday  |  10:00 - 11:00 A.M.  | Woodshop
+                                              | 11:30-12:30 A.M. |   Language Arts
+                                              | 9:00-11:00 A.M.   |   Social Studies
+                             Tuesday | 10:00-11:00 A.M.  |  Science"""
+
+#Here, I will make the output, otherwise there will be nothing but the process will happen.
+print(mycalendar)
